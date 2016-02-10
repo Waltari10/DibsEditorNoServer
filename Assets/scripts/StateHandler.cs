fad9a0820cdb96123888bc54b80dc9ab04a95c50
Hely Herranen <helyherranen@gmail.com>
2016-02-10 17:48:50 +0200

    highlights
    asdff

--- Assets/scripts/StateHandler.cs
+++ Assets/scripts/StateHandler.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class StateHandler : MonoBehaviour {
-	
+
+    Text teksti;
+    Text teksti1;
+    Text teksti2;
+    Text teksti3;
+    public Color color;
+    public Color notPressed;
+
 	public void TextScene ()
 	{
 		//  ladataan skenaario uudestaan vain jos sitä ei ole jo ladattu (ei voi avata samaa uudestaan)
 		if (UIScene.Instance.currentScenario != 0) {
-			UIScene.Instance.currentScenario = 0;
+            teksti = GameObject.Find("uitext").GetComponent<Text>();
+            teksti.color = color;
+            teksti1 = GameObject.Find("uicard").GetComponent<Text>();
+            teksti2 = GameObject.Find("uifont").GetComponent<Text>();
+            teksti3 = GameObject.Find("uisave").GetComponent<Text>();
+            teksti1.color = notPressed;
+            teksti2.color = notPressed;
+            teksti3.color = notPressed;
+            UIScene.Instance.currentScenario = 0;
 			Debug.Log (UIScene.Instance.currentScenario);
 			UIScene.Instance.UpdateScenario ();
 		}
@@ -17,7 +32,15 @@ public class StateHandler : MonoBehaviour {
 	{
 		//  ladataan skenaario uudestaan vain jos sitä ei ole jo ladattu (ei voi avata samaa uudestaan)
 		if (UIScene.Instance.currentScenario != 1) {
-			UIScene.Instance.currentScenario = 1;
+            teksti = GameObject.Find("uicard").GetComponent<Text>();
+            teksti.color = color;
+            teksti1 = GameObject.Find("uitext").GetComponent<Text>();
+            teksti2 = GameObject.Find("uifont").GetComponent<Text>();
+            teksti3 = GameObject.Find("uisave").GetComponent<Text>();
+            teksti1.color = notPressed;
+            teksti2.color = notPressed;
+            teksti3.color = notPressed;
+            UIScene.Instance.currentScenario = 1;
 			Debug.Log (UIScene.Instance.currentScenario);
 			UIScene.Instance.UpdateScenario ();
 		}
@@ -27,7 +50,15 @@ public class StateHandler : MonoBehaviour {
 	{
 		//  ladataan skenaario uudestaan vain jos sitä ei ole jo ladattu (ei voi avata samaa uudestaan)
 		if (UIScene.Instance.currentScenario != 2) {
-			UIScene.Instance.currentScenario = 2;
+            teksti = GameObject.Find("uifont").GetComponent<Text>();
+            teksti.color = color;
+            teksti1 = GameObject.Find("uicard").GetComponent<Text>();
+            teksti2 = GameObject.Find("uitext").GetComponent<Text>();
+            teksti3 = GameObject.Find("uisave").GetComponent<Text>();
+            teksti1.color = notPressed;
+            teksti2.color = notPressed;
+            teksti3.color = notPressed;
+            UIScene.Instance.currentScenario = 2;
 			Debug.Log (UIScene.Instance.currentScenario);
 			UIScene.Instance.UpdateScenario ();
 		}
@@ -37,7 +68,15 @@ public class StateHandler : MonoBehaviour {
 	{
 		//  ladataan skenaario uudestaan vain jos sitä ei ole jo ladattu (ei voi avata samaa uudestaan)
 		if (UIScene.Instance.currentScenario != 3) {
-			UIScene.Instance.currentScenario = 3;
+            teksti = GameObject.Find("uisave").GetComponent<Text>();
+            teksti.color = color;
+            teksti1 = GameObject.Find("uicard").GetComponent<Text>();
+            teksti2 = GameObject.Find("uifont").GetComponent<Text>();
+            teksti3 = GameObject.Find("uitext").GetComponent<Text>();
+            teksti1.color = notPressed;
+            teksti2.color = notPressed;
+            teksti3.color = notPressed;
+            UIScene.Instance.currentScenario = 3;
 			Debug.Log (UIScene.Instance.currentScenario);
 			UIScene.Instance.UpdateScenario ();
 		}
